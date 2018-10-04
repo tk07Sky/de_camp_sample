5e25e8039992776e139000c7c01307c4e086a6f8
木原大輝 <recode.seadex777@gmail.com>
2018-10-04 15:40:03 +0900

    refactor: いらない要素を削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,28 +5,6 @@
 </template>
 
 <script>
-import { getLanguage } from '~/utils/lang'
-import { mapActions } from 'vuex'
-
-export default {
-  mounted () {
-    let lang = this.getLanguage()
-    console.log(lang)
-    this.setLang(lang)
-  },
-  methods: {
-    ...mapActions('app', ['setLang']),
-    getLanguage () {
-      const browserLang = getLanguage()
-      if (browserLang === undefined) return 'ja'
-      if (browserLang.match(/en/)) {
-        return 'en'
-      } else {
-        return 'ja'
-      }
-    }
-  }
-}
 </script>
 
 
